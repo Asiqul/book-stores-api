a178c974822f77985fa3c9a5ac3aeb42a4cf9be3
Asiqul Hakim <hakimasiqul37@gmail.com>
2023-10-15 17:28:10 +0700

    fixing some bugs on paginations

--- src/controllers/books/populars.js
+++ src/controllers/books/populars.js
@@ -14,6 +14,7 @@ const popularBooks = async (page, limit) => {
                 },
             ],
         });
+
         const books = await prisma.books.findMany({
             skip: randomNumber || offset,
             take: limit ? limit : 10,
